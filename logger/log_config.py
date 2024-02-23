32d3056ff499637429a20200b2f728b030e8e838
Rajat Kumar R <rajat.kumar@rededucation.com>
2024-02-23 16:00:49 +0530

    Add logger.

---
+++ logger/log_config.py
@@ -0,0 +1,27 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import sys
+
+def setup_logger(name):
+    """
+    Configures and returns a logger with the specified name.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)  # Set the default logging level
+
+    # Check if handlers are already configured (e.g., when importing this function in multiple modules)
+    if not logger.handlers:
+        # Formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        # StreamHandler for stdout
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        # FileHandler for logging to a file
+        file_handler = TimedRotatingFileHandler('application.log', when='midnight', interval=1, backupCount=7)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
