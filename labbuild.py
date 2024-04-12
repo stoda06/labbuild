97fef73c2bc015ccada4f66785e57752e8510a0b
Rajat Kumar R <rajat.kumar@rededucation.com>
2024-04-12 14:42:51 +0530

    Add version and version check.

--- labbuild.py
+++ labbuild.py
@@ -163,8 +163,12 @@ def setup_environment(args):
                 futures = []
                 for pod in range(int(args.start_pod), int(args.end_pod) + 1):
                     pod_config = replace_placeholder(course_config, pod)
-                    build_futures = executor.submit(labs.avaya.build_aura_pod,
-                                        service_instance, pod_config)
+                    if pod_config["version"] == "aura":
+                        build_futures = executor.submit(labs.avaya.build_aura_pod,
+                                            service_instance, pod_config)
+                    if pod_config["version"] == "ipo":
+                        build_futures = executor.submit(labs.avaya.build_ipo_pod,
+                                            service_instance, pod_config)
                     futures.append(build_futures)
                 wait_for_futures(futures)
                 futures.clear()
