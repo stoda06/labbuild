f4436706822203a3e318bf9b46db7676f4f85643
Rajat Kumar R <rajat.kumar@rededucation.com>
2024-07-30 18:13:33 +0530

    Fix F5 bigip and w10 VM network update issue.

--- labs/setup/f5.py
+++ labs/setup/f5.py
@@ -5,6 +5,42 @@ from concurrent.futures import ThreadPoolExecutor
 from managers.network_manager import NetworkManager
 from managers.resource_pool_manager import ResourcePoolManager
 
+
+def update_network_dict(vm_name, network_dict, class_number, pod_number):
+    def replace_mac_octet(mac_address, pod_num):
+        mac_parts = mac_address.split(':')
+        mac_parts[4] = format(pod_num, '02x')  # This ensures zero-padded two-digit hex
+        return ':'.join(mac_parts)
+
+    class_vs = f"vs{class_number}"
+
+    updated_network_dict = {}
+    for adapter, details in network_dict.items():
+        network_name = details['network_name']
+        mac_address = details['mac_address']
+
+        if 'rdp' in network_name and 'apm' not in vm_name:
+            mac_address = replace_mac_octet(mac_address, pod_number)
+
+        if 'mgt' in network_name and 'srv-bigip' not in vm_name:
+            mac_address = replace_mac_octet(mac_address, pod_number)
+        
+        if 'vs0' in network_name:
+            network_name = network_name.replace('vs0', class_vs)
+        
+        if 'bigip' in vm_name and not 'srv-bigip' and ('ext' in network_name or 'int' in network_name):
+            mac_address = replace_mac_octet(mac_address, pod_number)
+
+        if 'w10' in vm_name and 'ext' in network_name:
+            mac_address = replace_mac_octet(mac_address, pod_number)
+
+        updated_network_dict[adapter] = {
+            'network_name': network_name,
+            'mac_address': mac_address
+        }
+
+    return updated_network_dict
+
 def build_class(service_instance, hostname, class_number, course_config):
     host = get_host_by_name(hostname)
     rpm = ResourcePoolManager(service_instance)
@@ -55,38 +91,28 @@ def build_class(service_instance, hostname, class_number, course_config):
 def build_srv(service_instance, class_number, parent_resource_pool, components, rebuild=False, thread=4, linked=False):
     vmm = VmManager(service_instance)
     for component in components:
+        clone_name = component["clone_vm"]+class_number
         if rebuild:
-            vmm.delete_vm(component["clone_vm"]+class_number)
-            vmm.logger.info(f"Deleted VM {component['clone_vm']}.")
+            vmm.delete_vm(clone_name)
+            vmm.logger.info(f"Deleted VM {clone_name}.")
         if linked:
             if not vmm.snapshot_exists(component["base_vm"], "base"):
                 vmm.create_snapshot(component["base_vm"], "base", 
                                     description="Snapshot used for creating linked clones.")
-            vmm.create_linked_clone(component["base_vm"], component["clone_vm"]+class_number, 
+            vmm.create_linked_clone(component["base_vm"], clone_name, 
                                     "base", parent_resource_pool)
-            vmm.logger.info(f'Created linked clone {component["clone_vm"]+class_number}.')
+            vmm.logger.info(f'Created linked clone {clone_name}.')
         else:
-            vmm.clone_vm(component["base_vm"], component["clone_vm"]+class_number, parent_resource_pool)
-            vmm.logger.info(f'Created direct clone {component["clone_vm"]+class_number}.')
+            vmm.clone_vm(component["base_vm"], clone_name, parent_resource_pool)
+            vmm.logger.info(f'Created direct clone {clone_name}.')
         # Step-3.2: Update VR Mac adderess and VM networks.
-        vm_network = vmm.get_vm_network(component["clone_vm"]+class_number)
-        specified_mac_address = "00:50:56:02:" + "{:02x}".format(int(class_number)) + ":0a" # Replace with the desired MAC address
-        updated_vm_network = {
-            k: {
-                sub_k: (
-                    sub_v.replace('0', str(class_number)) if sub_k == 'network_name' else specified_mac_address
-                ) if 'rdp' in v.get('network_name', '') and sub_k == 'mac_address' else (
-                    sub_v.replace('0', str(class_number)) if sub_k == 'network_name' else sub_v
-                )
-                for sub_k, sub_v in v.items()
-            }
-            for k, v in vm_network.items()
-        }
-        vmm.update_vm_network(component["clone_vm"]+class_number, updated_vm_network)
-        vmm.logger.info(f'Updated VM {component["clone_vm"]+class_number} networks.')
+        vm_network = vmm.get_vm_network(clone_name)
+        updated_vm_network = update_network_dict(clone_name, vm_network, int(class_number), int(class_number))
+        vmm.update_vm_network(clone_name, updated_vm_network)
+        vmm.logger.info(f'Updated VM {clone_name} networks.')
         if "bigip" in component["clone_vm"]:
-            vmm.update_serial_port_pipe_name(component["clone_vm"]+class_number, "Serial port 1",r"\\.\pipe\com_"+class_number)
-            vmm.logger.info(f'Updated serial port pipe name on {component["clone_vm"]+class_number}.')
+            vmm.update_serial_port_pipe_name(clone_name, "Serial port 1",r"\\.\pipe\com_"+class_number)
+            vmm.logger.info(f'Updated serial port pipe name on {clone_name}.')
     # Step-3.3: Power-on all VMs.
     vmm.logger.info(f'Power on all components in {parent_resource_pool}.')
     for component in components:
@@ -103,48 +129,53 @@ def build_srv(service_instance, class_number, parent_resource_pool, components, 
                 except Exception as e:
                     vmm.logger.error(f'Error powering on VM: {e}')
 
-def build_pod(service_instance, class_number, parent_resource_pool, components, pod, rebuild=False, thread=4, linked=False):
+def build_pod(service_instance, class_number, parent_resource_pool, components, pod_number, rebuild=False, thread=4, linked=False):
     vmm = VmManager(service_instance)
     # Step-3.1: Clone components.
     for component in components:
+        if 'w10' in component["clone_vm"]:
+            clone_name = component["clone_vm"].format(X=pod_number)
+        else:
+            clone_name = component["clone_vm"] + pod_number
         if rebuild:
-            vmm.delete_vm(component["clone_vm"]+pod)
-            vmm.logger.info(f'Deleted VM {component["clone_vm"]+pod}.')
+            vmm.delete_vm(clone_name)
+            vmm.logger.info(f'Deleted VM {clone_name}.')
         if linked:
             if not vmm.snapshot_exists(component["base_vm"], "base"):
                 vmm.create_snapshot(component["base_vm"], "base", 
                                     description="Snapshot used for creating linked clones.")
-            vmm.create_linked_clone(component["base_vm"], component["clone_vm"]+pod, 
+            vmm.create_linked_clone(component["base_vm"], clone_name, 
                                     "base", parent_resource_pool)
-            vmm.logger.info(f'Created linked clone {component["clone_vm"]+pod}.')
+            vmm.logger.info(f'Created linked clone {clone_name}.')
         else:
-            vmm.clone_vm(component["base_vm"], component["clone_vm"]+pod, parent_resource_pool)
-            vmm.logger.info(f'Created direct clone {component["clone_vm"]+pod}.')
+            vmm.clone_vm(component["base_vm"], clone_name, parent_resource_pool)
+            vmm.logger.info(f'Created direct clone {clone_name}.')
+
         # Step-3.2: Update VR Mac adderess and VM networks.
-        vm_network = vmm.get_vm_network(component["clone_vm"]+pod)
-        specified_mac_address = "00:50:56:02:" + "{:02x}".format(int(class_number)) + ":0a" # Replace with the desired MAC address
-        updated_vm_network = {
-            k: {
-                sub_k: (
-                    sub_v.replace('0', str(class_number)) if sub_k == 'network_name' else specified_mac_address
-                ) if 'rdp' in v.get('network_name', '') and sub_k == 'mac_address' else (
-                    sub_v.replace('0', str(class_number)) if sub_k == 'network_name' else sub_v
-                )
-                for sub_k, sub_v in v.items()
-            }
-            for k, v in vm_network.items()
-        }
-        vmm.update_vm_network(component["clone_vm"]+pod, updated_vm_network)
-        vmm.logger.info(f'Updated VM {component["clone_vm"]+pod} networks.')
-        if "bigip" in component["clone_vm"] or "w10" in component["clone_vm"]:
-            vmm.update_serial_port_pipe_name(component["clone_vm"]+pod, "Serial port 1",r"\\.\pipe\com_"+pod)
-            vmm.logger.info(f'Updated serial port pipe name on {component["clone_vm"]+pod}.')
+        vm_network = vmm.get_vm_network(clone_name)
+        update_vm_network = update_network_dict(clone_name, vm_network, class_number, int(pod_number))
+        vmm.update_vm_network(clone_name,update_vm_network)
+        vmm.logger.info(f'Updated VM {clone_name} networks.')
+        if "bigip" in clone_name or "w10" in clone_name:
+            vmm.update_serial_port_pipe_name(clone_name, "Serial port 1",r"\\.\pipe\com_"+pod_number)
+            vmm.logger.info(f'Updated serial port pipe name on {clone_name}.')
+            if "w10" in clone_name:
+                drive_name = "CD/DVD drive 1"
+                iso_type = "Datastore ISO file"
+                datastore_name = "vms"
+                iso_path = "podiso/pod-"+pod_number+"-a.iso"
+                vmm.modify_cd_drive(clone_name, drive_name, iso_type, datastore_name, iso_path, connected=True)
+
     # Step-3.3: Power-on all VMs.
     vmm.logger.info(f'Power on all components in {parent_resource_pool}.')
     for component in components:
+        if 'w10' in component["clone_vm"]:
+            clone_name = component["clone_vm"].format(X=pod_number)
+        else:
+            clone_name = component["clone_vm"] + pod_number
         with ThreadPoolExecutor(max_workers=thread) as executor:
             tasks = [
-                executor.submit(vmm.poweron_vm, component["clone_vm"]+pod)
+                executor.submit(vmm.poweron_vm, clone_name)
                 for component in components
                 if "state" not in component or "poweroff" not in component["state"]
             ]
