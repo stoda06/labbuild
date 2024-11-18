7e24eacd0c1086870dd5acaf6c6648c573741905
Rajat Kumar R <rajat.kumar@rededucation.com>
2024-11-18 20:43:14 +0530

    Look for '0' to update PAN networks.

--- labs/setup/palo.py
+++ labs/setup/palo.py
@@ -13,7 +13,7 @@ def update_network_dict_1110(network_dict, pod_number):
             details['mac_address'] = ':'.join(mac_parts)
         else:
             details['network_name'] = details['network_name'].replace('pa-', 'pan-')
-            details['network_name'] = details['network_name'].replace('1', str(pod_number))
+            details['network_name'] = details['network_name'].replace('0', str(pod_number))
 
     return network_dict
 
