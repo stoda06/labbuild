469c8ad3163fbe2f8a73bba395889e96e4c8b648
Rajat Kumar R <rajat.kumar@rededucation.com>
2025-02-21 15:26:27 +0530

    Fix network name replacement issue for pods ending with '0'.

--- labs/setup/palo.py
+++ labs/setup/palo.py
@@ -17,7 +17,8 @@ def update_network_dict_1110(network_dict, pod_number):
             details['network_name'] = details['network_name'].replace('pa-', 'pan-')
             if 'pan-internal-1' in details['network_name']:
                 details['network_name'] = details['network_name'].replace('1', str(pod_number))
-            details['network_name'] = details['network_name'].replace('0', str(pod_number))
+            else:
+                details['network_name'] = details['network_name'].replace('0', str(pod_number))
 
     return network_dict
 
