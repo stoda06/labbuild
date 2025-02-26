4304bd3c9b502ba78658fbf371d566f03058dd05
Rajat Kumar R <rajat.kumar@rededucation.com>
2025-02-26 11:28:03 +0530

    Fix nu teardown.

--- labbuild.py
+++ labbuild.py
@@ -564,8 +564,7 @@ def vendor_teardown(service_instance, host_details, args, course_config):
                 elif vendor == "prtg":
                     teardown_func = pr.teardown_pr_pod
                 elif vendor == "nu":
-                    teardown_func = nu.teardown_nu_pod
-                    continue
+                    teardown_func = nu.teardown_nu_pod   
                 else:
                     logger.error("Unsupported vendor for teardown: %s", vendor)
                     continue
