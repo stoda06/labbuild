b6444cb872e6839524abc6f690b388a54af56961
Rajat Kumar R <rajat.kumar@rededucation.com>
2025-04-16 03:44:53 +0530

    Delete unused files.

--- database/functions.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from urllib.parse import quote_plus
-
-MONGO_USER = quote_plus("labbuild_user")
-MONGO_PASSWORD = quote_plus("$$u1QBd6&372#$rF")
-MONGO_HOST = os.getenv("MONGO_HOST")
-MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:27017/labbuild_db"
